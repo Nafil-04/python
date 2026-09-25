fc2f5dec19cbbfdce22863b86fa4a4ca831df6c1
Nafil-04 <areekadannafil58@gmail.com>
2026-09-25 11:21:19 +0530

    added list.py

--- list.py
+++ list.py
@@ -1,1 +1,68 @@
-a=["apple","banana","cherry"]
+# creating a list
+# my_list=[1,2,3,'python',4.5]
+# print(my_list)
+
+# accessing list items
+# my_list=['apple','banana','cherry']
+# print(my_list[1])
+# print(my_list[-1])
+
+# changing list items
+# my_list[1]='orange'
+# print(my_list)
+
+# my_list[1:3]=['orange','grape']
+# print(my_list)
+
+# my_list.insert(1,'cherry')
+# print(my_list)
+
+# adding items to a list
+# my_list.append('grape')
+# print(my_list)
+
+# new_list=['orange','grape']
+# my_list.extend(new_list)
+# print(my_list)
+
+# removing items from a list
+# my_list.remove('banana')
+# print(my_list)
+
+# popped_item=my_list.pop(1)
+# print(popped_item)
+# print(my_list)
+
+# del my_list[0]
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+# list methods
+# my_list=[1,2,3,2,4,2]
+# print(my_list.count(2))
+# print(my_list.index(2))
+
+# my_list.reverse()
+# print(my_list)
+
+# my_list.sort()
+# print(my_list)
+
+# copied_list=my_list.copy()
+# print(copied_list)
+
+# sorting a list
+# numbers=[3,5,1,6,2,4]
+# numbers.sort()
+# print(numbers)
+# numbers.sort(reverse=True)
+# print(numbers)
+
+# joining lists
+# list1 =['apple','cherry']
+# list2 =['grape','banana']
+# list1.extend(list2)
+# print(list1)
+
